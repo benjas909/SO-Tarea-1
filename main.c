1a46e15e500e2e2cec2308ce0a735361bd259e8f
benjas <benjamin.aguilera64@gmail.com>
2024-03-18 22:17:53 -0300

    Agrega función createFolder
    también arregla función doesFolderExist

--- main.c
+++ main.c
@@ -37,16 +37,25 @@ const char *currDir()
  * @param name
  * @return int
  */
-int doesFolderExist(char name)
+int doesFolderExist(char *name, int length)
 {
 
-  char lsCommand[5] = "ls x";
+  char lsCommand[5] = "ls ";
 
-  lsCommand[3] = name;
+  strcat(lsCommand, name);
+  // lsCommand[3] = name;
 
   int result = system(lsCommand) / 256;
 
   return !result;
+}
+
+void createFolder(char *name, int nameLen)
+{
+  char mkdirCommand[9] = "mkdir ";
+  strcat(mkdirCommand, name);
+
+  system(mkdirCommand);
 }
 
 int main()
@@ -57,14 +66,17 @@ int main()
 
   int ind;
 
-  char currLetter;
+  char currLetter[2];
+  currLetter[1] = '\0';
 
   for (ind = 0; ind < 26; ind++)
   {
-    currLetter = toupper(ALPHABET[ind]);
+    currLetter[0] = toupper(ALPHABET[ind]);
     // printf("%c\n", currLetter);
 
-    printf("%d\n", doesFolderExist(currLetter));
+    printf("%d\n", doesFolderExist(currLetter, 1));
+
+    createFolder(currLetter, 1);
   }
 
   return 0;
